7c203d4a2c2f54cefab78e98842cdb9fb6f43092
Mohamed Elsayed <moelsayed524@gmail.com>
2025-11-21 20:26:20 +0200

    propotions as course and convert

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import StarryBackground from "./components/common/StarryBackground";
 import Header from "./components/common/Header";
 import Hero from "./components/sections/Hero";
-import Programs from "./components/sections/Programs";
-import Bonuses from "./components/sections/Bonuses";
 import SkillStacking from "./components/sections/SkillStacking";
 import Testimonials from "./components/sections/Testimonials";
 import Pricing from "./components/sections/Pricing";
@@ -16,8 +14,6 @@ export default function App() {
       <Header />
       <main className="relative z-10">
         <Hero />
-        {/* <Programs /> */}
-        {/* <Bonuses /> */}
         {/* <SkillStacking /> */}
         <Testimonials />
         {/* <Pricing /> */}
